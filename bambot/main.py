d609060a55772b1e59cb3339643069f832db84b2
DanielVZ96 <dsvalenzuela@uc.cl>
2019-06-02 15:42:48 -0400

    no use for dotenv

--- bambot/main.py
+++ bambot/main.py
@@ -1,9 +1,5 @@
-from dotenv import load_dotenv
-from pathlib import Path
 from .botmaster import TwitchBotMaster
 
 if __name__ == '__main__':
-    dot_path = Path('/home/bam/bambot/.env')
-    load_dotenv(verbose=True, dotenv_path=dot_path)
     twitch_master = TwitchBotMaster()
     twitch_master.run()

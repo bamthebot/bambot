386c288377643eb4fdae3ffed3271540e15bc24a
Daniel Valenzuela <daniel@admetricks.com>
2019-07-08 08:44:48 -0400

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 TAG=$1
+
+# Build
+cd ..
+docker build . -t bambot
+
+# Push
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD 
 docker tag bambot twitchbambot/bambot:$TAG
 docker push twitchbambot/bambot:$TAG

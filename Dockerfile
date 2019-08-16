37d3a55526fbfeab55a6ff95f6ee899e1790d62e
DanielVZ <dowy.vz6@gmail.com>
2019-08-16 17:48:12 -0400

    changes (#2)

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ ADD tests /home/bam/bambot/tests
 WORKDIR /home/bam/
 RUN python3 -m venv bambot/venv && . bambot/venv/bin/activate
 RUN pip install -r bambot/requirements.txt
-CMD python -m bambot.main
+CMD ["python", "-m", "bambot.main"]
--- bambot/bot.py
+++ bambot/bot.py
@@ -3,14 +3,24 @@ import websockets
 
 from .bangs.common import BangSet
 from .bangs.common import SpecialBangSet
+from .bangs.skills import SkillSet
 from .irc.twitch_irc import TwitchIrcConnection
 
 
 class TwitchBot:
-    nick = os.getenv('BOT_NICKNAME', 'bamthebot')
-    token = os.getenv('BOT_TOKEN')
-    bang_prefix = os.getenv('COMMAND_PREFIX', '!')
-    not_found_response = os.getenv('NOT_FOUND_RESPONSE', 'Bang not found, please try with: ')
+    class ControlSkillSet(SkillSet):
+        def __init__(self, user_id, controls):
+            self.user_id = user_id
+            for control in controls:
+                setattr(self, control.__name__, control)
+            self._get_skills()
+
+    nick = os.getenv("BOT_NICKNAME", "bamthebot")
+    token = os.getenv("BOT_TOKEN")
+    bang_prefix = os.getenv("COMMAND_PREFIX", "!")
+    not_found_response = os.getenv(
+        "NOT_FOUND_RESPONSE", "Bang not found, please try with: "
+    )
 
     def __init__(self, user):
         self.user = user
@@ -20,25 +30,34 @@ class TwitchBot:
         self.irc_connection = TwitchIrcConnection(self.channel, self.nick, self.token)
         self.user_bang_set = BangSet(self.user_id)
         self.user_special_bang_set = SpecialBangSet(self.user_id)
+        self.control_bang_set = SpecialBangSet(
+            self.user_id,
+            skill_set=self.ControlSkillSet(self.user_id, [self.mute, self.help]),
+        )
+        self.muted = False
+        self.just_muted = False
         self.connecting = False
 
     async def handle_connection(self):
         self.connecting = True
         if self.irc_connection.websocket is None:
-            print('Websocket not instantiated. Creating websocket.')
+            print("Websocket not instantiated. Creating websocket.")
             await self.irc_connection.connect()
         while self.irc_connection.websocket.open:
-            print(f'Handling {self.channel}\'s connection.')
+            print(f"Handling {self.channel}'s connection.")
             try:
                 async for message in self.irc_connection:
                     await self.handle_message(message)
             except websockets.exceptions.ConnectionClosed:
+                print("Websocket connection closed. Reconnecting websocket.")
                 await self.irc_connection.connect()
 
     async def handle_message(self, message):
         if self.is_bang(message):
             response = self.get_response(message)
-            await self.irc_connection.send_channel_message(response)
+            if not self.muted or self.just_muted:
+                self.just_muted = False
+                await self.irc_connection.send_channel_message(response)
         elif self.irc_connection.is_ping(message):
             await self.irc_connection.send_pong()
 
@@ -50,38 +69,60 @@ class TwitchBot:
         return False
 
     def get_response(self, message):
-        print('Updating Bangs!')
+        print("Updating Bangs!")
         self.user_bang_set.update()
-        print('Getting response for', message)
-        message = message.strip().strip(self.bang_prefix).split(' ')
+        print("Getting response for", message)
+        message = message.strip().strip(self.bang_prefix).split(" ")
         command = message[0]
-        print('Command is', command, command in self.user_bang_set)
+        print("Command is", command, command in self.user_bang_set)
         command_args = None
         if len(message) > 1:
             command_args = message[1:]
         response = None
 
-        if command in self.user_special_bang_set:
-            print('Command is special')
-            response = self.user_special_bang_set[command]
-            if command_args is not None:
-                args = ' '.join(command_args)
-                response = response.process_args(args)
-            else:
-                response = response.process_args()
+        if command in self.control_bang_set:
+            response = self.control_bang_set.process_command(command, command_args)
+        elif command in self.user_special_bang_set:
+            print("Command is special")
+            response = self.user_special_bang_set.process_command(command, command_args)
         elif command in self.user_bang_set:
-            print('Command is normal')
+            print("Command is normal")
             response = self.user_bang_set[command]
         else:
-            response = self.not_found_response + ', '.join(self.get_commands())
+            response = self.not_found_response + ", ".join(self.get_commands())
 
         if self.is_bang(response):
             return self.get_response(response)
-
         return response
 
     def get_commands(self):
-        return list(self.user_special_bang_set) + list(self.user_bang_set)
+        return (
+            list(self.user_special_bang_set)
+            + list(self.user_bang_set)
+            + list(self.control_bang_set)
+        )
 
     async def close_connection(self):
         await self.irc_connection.disconnect()
+
+    # Control Skills
+    def mute(self, *args, **kwargs):
+        """Makes the bot shut up"""
+        self.muted = not self.muted  # toogle
+        if self.muted:
+            self.just_muted = True
+            return "I've been muted :("
+        return "I'm back! :D"
+
+    def help(self, *args, **kwargs):
+        """Displays the help text for a special command"""
+        if len(args) == 0:
+            return self.help.__doc__
+        docs = []
+        for command in args:
+            if command in self.user_special_bang_set:
+                docs.append(self.user_special_bang_set[command].bang_skill.__doc__)
+            elif command in self.control_bang_set:
+                docs.append(self.control_bang_set[command].bang_skill.__doc__)
+
+        return "| ".join(docs)

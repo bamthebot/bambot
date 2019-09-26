7049d47ae27d7c58c1b8c9de8292e2382de7659c
Daniel Valenzuela <daniel@admetricks.com>
2019-09-25 22:40:09 -0300

    skills tests

--- tests/test_skills.py
+++ tests/test_skills.py
@@ -13,3 +13,20 @@ async def test_leaderboard():
     tier_one = TierOneSkillSet()
     response = await tier_one.leaderboard("botw/Any%")
     assert "1)" in response and "2)" in response
+
+
+@pytest.mark.asyncio
+async def test_worldrecord():
+    tier_one = TierOneSkillSet()
+    response = await tier_one.worldrecord("botw/Any%")
+    assert "The WR for botw's Any% is" in response
+
+
+@pytest.mark.asyncio
+async def test_personalbests():
+    tier_one = TierOneSkillSet()
+    response = await tier_one.personalbests("Wolhaiksong")
+    assert "Personal bests for Wolhaiksong are: " in response
+
+    response = await tier_one.personalbest("Wolhaiksong/botw/Any%")
+    assert "Personal bests for Wolhaiksong are: " in response
